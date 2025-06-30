ab6f3be3f11ee9437995357c88f894ffb4846289
Marcin Wojtach <marcin.vojtach@gmail.com>
2025-06-30 20:35:25 +0200

    add migration snapshot for the item model

--- RetroSum/Migrations/20250628105328_AddContentColumn.Designer.cs
+++ RetroSum/Migrations/20250628105328_AddContentColumn.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RetroSum.Data.Context;
 
@@ -10,9 +11,11 @@ using RetroSum.Data.Context;
 namespace RetroSum.Migrations
 {
     [DbContext(typeof(RetroSumContext))]
-    partial class RetroSumContextModelSnapshot : ModelSnapshot
+    [Migration("20250628105328_AddContentColumn")]
+    partial class AddContentColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.6");
---
+++ RetroSum/Migrations/20250628105328_AddContentColumn.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RetroSum.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddContentColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Content",
+                table: "Retros",
+                type: "TEXT",
+                maxLength: 10000,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "Retros");
+        }
+    }
+}
